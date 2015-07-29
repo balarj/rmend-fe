53a3ce55688b8b0503e468f9917a03438cef4ed3
bxr4261 <bxr4261@rit.edu>
2015-07-28 21:12:12 -0700

    sending PUT request for user impression

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var App = function() {
 
+	console.log("initializing app");
+
 	this.topics = {};
 	this.currentTopic = null;
-	this.uuid = null;
+	this.uuid = null; //c3b1abc4-0b87-4d76-be09-b7440de2f690
+	this.uid = null;
 	this.initUI();
 
 	//refactor this into a fn()
@@ -13,34 +16,14 @@ var App = function() {
 	} else {
 		this.greet();
 	}
-
 }
 
 App.prototype = {
 	constructor:App,
 	initUI: function(){ 
-		console.log("initUI");
-
 		var instance = this;
 
-		$("#docs-by-topic").on("click",'a',function(e) {
-			var i = $(this).index();
-			console.log("doc clicked: " + i);
-
-			if(instance.currentTopic && instance.topics) {
-				var documents = instance.topics[instance.currentTopic];	
-				var doc = documents[i];
-				console.log(doc);
-				var lightBoxHTML = '<h2>'+doc.title+'</h2><div class="margin-top">'+doc.docBody+'</div>';
-				$("#mylightbox").html(lightBoxHTML);
-
-				// Register user impression
-				// Calls to get recommendations (content and item based)
-			}
-			
-
-		});
-
+		// topic click handler
 		$(".topic").on("click", function(e) {
 			e.preventDefault();
 			var topic = $(this).attr('topic');
@@ -49,6 +32,26 @@ App.prototype = {
 				instance.currentTopic = topic;
 				instance.getDocumentsByTopic(topic);
 			}
+		});
+
+		// document (under topic) click handler
+		$("#docs-by-topic").on("click",'a',function(e) {
+			var index = $(this).index();
+			console.log("doc clicked | index: " + index);
+
+			var doc = instance.getDocumentAtIndex(index);
+			if(doc) {
+				var lightBoxHTML = '<h2>'+doc.title+'</h2><div class="margin-top">'+doc.docBody+'</div>';
+				$("#mylightbox").html(lightBoxHTML);
+
+				// user impression
+				if(instance.uid) {
+					instance.sendUserImpression(instance.uid,doc.docNum);
+				}
+			}
+			
+			// Calls to get recommendations (content and item based)	
+	
 		});
 
 	},
@@ -64,8 +67,9 @@ App.prototype = {
 
 		$.ajax(settings).done(function (response) {
 		  console.log(response);
-		  if(response.uuid && response.userName) {
+		  if(response.uuid && response.uid && response.userName) {
 		  	instance.uuid = response.uuid;
+		  	instance.uid = response.uid;
 		  	instance.greet(response.userName);	
 		  }
 		  
@@ -87,7 +91,6 @@ App.prototype = {
 
 		$("#welcome-greeting").text(msg);		
 	},
-
 	getDocumentsByTopic:function(topic) {
 		var instance = this;
 		var settings = {
@@ -107,32 +110,87 @@ App.prototype = {
 			console.log("Error: topic get");
 		});
 	},
-
+	getDocumentAtIndex:function(index) {
+		if(this.currentTopic && this.topics) {
+			var documents = this.topics[this.currentTopic];	
+			var doc = documents[index];
+			return doc;
+		}
+		return null;
+	},
 	updateTopicDocumentsView:function() {
 		$("#docs-by-topic").empty();
-		console.log("updateTopicDocumentsView");
+		
 		var currentTopic = this.currentTopic;
 		var topics = this.topics;
 
 		if(currentTopic && topics) {
-			console.log("A");
 			var i, doc;
 			var documents = topics[currentTopic];
 			for(i in documents) {
 				doc = documents[i]
 				console.log(doc);
-
 				var docHTML = '<a href=#><div class="doc" data-featherlight="#mylightbox">' + doc.title.substring(0, 15); + '</div></a>'
 				$("#docs-by-topic").append(docHTML);
 
 			}	
 		}
 	},
+	sendUserImpression:function(uid,docNum){
+		var settings = {
+		  "async": true,
+		  "crossDomain": true,
+		  "url": Config.BASE_URL + "/view/impression?referrer=POSTMAN-CLIENT",
+		  "method": "PUT",
+		  "headers": {
+		    "content-type": "application/json"
+		  },
+		  "processData": false,
+		  data : JSON.stringify({
+		  	"uid":uid,
+			"docNum":docNum
+		  })
+		}
 
+		console.log(settings);
+
+		$.ajax(settings).done(function (response) {
+		  console.log(response);
+		}).fail(function(error){
+			console.log("Error: user impression PUT");
+		});
+		
+
+		// XHR
+		// ---------------------------
+		/* 
+		var data = JSON.stringify({
+		  "uid": 7000001,
+		  "docNum": 4646595069
+		});
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function () {
+		  if (this.readyState === this.DONE) {
+		    console.log(this.responseText);
+		  }
+		});
+
+		xhr.open("PUT", "http://rmend-be.appspot.com//v1/view/impression?referrer=POSTMAN-CLIENT");
+		xhr.setRequestHeader("content-type", "application/json");
+
+		xhr.send(data);
+		*/
+		// ---------------------------
+
+
+	},
 	getURLParameter: function(name) {
 	  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
 	}
-}
+}	
 
 // ------------------------------------------
 // initialize app
@@ -140,8 +198,6 @@ App.prototype = {
 var app;
 
 $(document).ready(function(){
-	console.log("ready");
 	app = new App();
-
 });
 
